1306cfdd73b1191e3835dc2f93bb621238fc5a65
John Akpa Ojonugwa <johnakpaojonugwa@gmail.com>
2025-12-11 21:45:41 +0100

    edited my teacher's routes

--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -6,7 +6,7 @@ import { authorize, protect } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // Route to register a new teacher
-router.post('/create', uploadMiddleware, createTeacher);
+router.post('/create', protect, authorize('admin'), uploadMiddleware, createTeacher);
 //Route to login teacher
 router.post('/login', loginTeacher);
 // Route to get all teachers
@@ -14,7 +14,7 @@ router.get('/', getAllTeachers);
 //Route to get single teacher
 router.get('/:teacherId', getSingleTeacher);
 //Route to update teacher details
-router.put('/:teacherId', uploadMiddleware, updateTeacher);
+router.put('/:teacherId', protect, authorize('admin'), uploadMiddleware, updateTeacher);
 //route to logout teacher
 router.post('/logout/:teacherId', logoutTeacher);
 //route to delete teacher
